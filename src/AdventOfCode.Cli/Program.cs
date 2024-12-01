4587ca7021a0a639cce171a5f10357331879949d
Daniel Newton <kdaniel.newton@gmail.com>
2024-12-01 12:35:08 -0600

    Add CLI project

---
+++ src/AdventOfCode.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using Cocona;
+
+var builder = CoconaApp.CreateBuilder();
+
+var app = builder.Build();
+
+app.AddCommand("solve", (int year) =>
+{
+    Console.WriteLine($"\"Solving\" for {year}");
+});
+
+app.Run();
