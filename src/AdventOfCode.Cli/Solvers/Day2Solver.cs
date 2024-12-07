9b8aeb08307311111bb20e5c9fc3969495747e3c
Daniel Newton <daniel.newton@payscale.com>
2024-12-06 22:10:37 -0600

    somewhere in day 2

---
+++ src/AdventOfCode.Cli/Solvers/Day2Solver.cs
@@ -0,0 +1,87 @@
+﻿namespace AdventOfCode.Cli.Solvers;
+
+public class Day2Solver : ISolver
+{
+    public string SolvePartA(string[] input)
+    {
+        // The levels are either all increasing or all decreasing.
+        // Any two adjacent levels differ by at least one and at most three.
+
+        var safeCount = input.Select(line => line.Split(' ').Select(int.Parse).ToList())
+            .Count(IsSafe);
+
+        return safeCount.ToString();
+    }
+
+    public string SolvePartB(string[] input)
+    {
+        var safeCount = input.Select(line => line.Split(' ').Select(int.Parse).ToList())
+            .Count(IsSafeWithDampener);
+
+        return safeCount.ToString();
+    }
+
+    private static bool IsSafe(List<int> levels)
+    {
+        bool? increasing = null;
+        for (var i = 1; i < levels.Count; i++)
+        {
+            var diff = levels[i] - levels[i - 1];
+            
+            var absDiff = Math.Abs(diff);
+            if (absDiff is < 1 or > 3) return false;
+
+            if (diff > 0)
+            {
+                increasing ??= true;
+                if (!increasing.Value) return false;
+            }
+            else
+            {
+                increasing ??= false;
+                if (increasing.Value) return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool IsSafeWithDampener(List<int> levels)
+    {
+        // if removing a single level from an unsafe report would make it safe, the report instead counts as safe
+        bool dampenerUsed = false;
+        bool? increasing = null;
+        for (var i = 1; i < levels.Count; i++)
+        {
+            var diff = levels[i] - levels[i - 1];
+            
+            var absDiff = Math.Abs(diff);
+            if (absDiff is < 1 or > 3)
+            {
+                if (dampenerUsed) return false;
+                dampenerUsed = true;
+            }
+
+            if (diff > 0)
+            {
+                increasing ??= true;
+                if (!increasing.Value)
+                {
+                    if (dampenerUsed) return false;
+                    dampenerUsed = true;
+                }
+            }
+            else
+            {
+                increasing ??= false;
+                if (increasing.Value)
+                {
+                    if (dampenerUsed) return false;
+                    dampenerUsed = true;
+                }
+            }
+        }
+
+        return true;
+    }
+}
